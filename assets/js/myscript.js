2c841f1b896306ea927b3ed3c7e81c0be59554d6
smnwltr <smn@wltr.co>
2019-08-31 00:24:00 +0200

    Basic jekyll & bootstrap setup with one-page scaffold

---
+++ assets/js/myscript.js
@@ -0,0 +1,44 @@
+
+// Hamburger menu toggling
+window.onload = function () {
+    // Look for .hamburger
+    var hamburger = document.querySelector(".hamburger");
+    var elementsArray = document.querySelectorAll(".nav-link");
+    var collapsible = document.getElementById("navbarNav")
+    // On click toggle class "is-active"
+    hamburger.addEventListener("click", function () {
+        hamburger.classList.toggle("is-active");
+    });
+
+    elementsArray.forEach(function (elem) {
+        elem.addEventListener("click", function () {
+            hamburger.classList.remove("is-active");
+            collapsible.classList.remove("show");
+        });
+    });
+}
+
+
+// smooth scrolling
+$(document).ready(function () {
+    $(".nav-item a[href^='#']").click(function (e) {
+        e.preventDefault();
+
+        var position = $($(this).attr("href")).offset().top;
+
+        $("html, body").animate({
+            scrollTop: position
+        }, 1000);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.scrolltop:hidden').stop(true, true).fadeIn();
+        } else {
+            $('.scrolltop').stop(true, true).fadeOut();
+        }
+    });
+    $(function () { $(".scroll").click(function () { $("html,body").animate({ scrollTop: 0 }, 1000); return false }) })
+
+});
+
